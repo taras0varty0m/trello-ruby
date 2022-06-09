c4e27ca16a1c6c3157d2049fd5540c27470a223b
Artyom Tarasov <tarasovar457@gmail.com>
2022-06-09 18:47:45 +0600

    refactor: remove brackets

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 password = Faker::Internet.password
-user = User.create({ email: Faker::Internet.email, password: password, password_confirmation: password })
-column = user.columns.create({ title: Faker::Book.title, user_id: user.id })
-card = column.cards.create({ title: Faker::Book.title, description: Faker::Lorem.sentence, user_id: user.id })
-card.comments.create({ text: Faker::Lorem.paragraph, user_id: user.id })
+user = User.create(email: Faker::Internet.email, password: password, password_confirmation: password)
+column = user.columns.create(title: Faker::Book.title, user_id: user.id)
+card = column.cards.create(title: Faker::Book.title, description: Faker::Lorem.sentence, user_id: user.id)
+card.comments.create(text: Faker::Lorem.paragraph, user_id: user.id)
